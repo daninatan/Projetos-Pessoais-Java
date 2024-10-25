92ff63a51d3e3c3cee4808fcdca7a66cb45eb0b6
daninatan <danielnatanbrito@hotmail.com>
2024-10-25 08:42:56 -0300

    2º Commit - Adição Projeto Restaurante

---
+++ Sistema_Restaurante/Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente {
+
+    private String nome;
+
+    public Cliente(){
+        this.nome = "Cliente";
+    }
+
+    public Cliente(String nome){
+        this.nome = nome;
+    }
+}
---
+++ Sistema_Restaurante/Mesa.java
@@ -0,0 +1,23 @@
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Mesa {
+
+    private ArrayList<Cliente> clientesNaMesa = new ArrayList<Cliente>();
+
+    private int pessoasNaMesa;
+    private int numeroDaMesa;
+
+    public Mesa(int numeroDaMesa){
+        this.numeroDaMesa = numeroDaMesa;
+    }
+
+    public void adicionarCliente(Cliente cliente){
+        if(!clientesNaMesa.contains(cliente)){
+            clientesNaMesa.add(cliente);
+        }else{
+            System.out.println("Erro, cliente ja esta na mesa");
+        }
+    }
+}
